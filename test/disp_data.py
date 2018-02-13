ae9307af6d0aea3d83993583d057d9ac31b78f66
Hikaru shimoe <lettel@HikarunoMacBook-Pro.local>
2018-02-13 10:41:48 +0900

    make test dir

---
+++ test/disp_data.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+#データの入ったファイルパス指定
+#一度.xlsx形式をexcelで開けた後/txtファイル形式(tab区切り)でデータを読み込む
+
+over_sample_num = 1000#タイムフィルタサイズ
+sample_rate = 1000
+filename ="./test_001.lvm"
+data_size = 3
+
+csv = pandas.read_csv(filename, sep='\t', encoding="shift-jis", error_bad_lines=False)
+data = np.array(csv.ix[ : , 1:data_size+1],dtype = np.float32 ).T
+print(data)
+conv = np.ones(over_sample_num)
+for i in range(data_size):
+	tmp = np.convolve(data[i], conv, mode = 'full')
+	tmp = tmp[over_sample_num:int(data.shape[0])-over_sample_num]/over_sample_num
+	x = np.array(range(tmp.shape[0]))/sample_rate
+	plt.plot(x, tmp)
+plt.xlabel("time(sec)")
+plt.ylabel("voltage")
+
+plt.show() #見たければどうぞ
